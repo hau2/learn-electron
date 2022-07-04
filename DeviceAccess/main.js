10bc55ca880bf9fc752ee95a0c89c06182b0130d
Hau. Le Cong - CMC Global DJ7 <=>
2022-07-04 17:40:24 +0700

    commit file

---
+++ DeviceAccess/main.js
@@ -0,0 +1,32 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const { webContents } = require('electron')
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  mainWindow.webContents.on('select-bluetooth-device', (event, deviceList, callback) => {
+    event.preventDefault()
+    if (deviceList && deviceList.length > 0) {
+      callback(deviceList[0].deviceId)
+    } 
+  })
+
+  mainWindow.loadFile('index.html')
+  console.log(mainWindow.webContents);
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
---
+++ NactiveFileDragDrop/renderer.js
@@ -0,0 +1,4 @@
+document.getElementById('drag').ondragstart = (event) => {
+    event.preventDefault()
+    window.electron.startDrag('drag-and-drop.md')
+  }
